5dfd425d0629d6a97c9a5908e99cb14f0e3c8b44
Martin-Mas <MAS.US@Outlook.com>
2021-05-20 22:33:13 +0800

    调整系统布局自适应！

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <Asider />
+    <div class="main-container">
+      <Header />
+      <div class="app-main">
+        <Mainer />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Asider from "./Views/Layout/Asider.vue";
+import Header from "./Views/Layout/Header.vue";
+import Mainer from "./Views/Layout/Mainer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Asider,
+    Header,
+    Mainer,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+.main-container {
+  flex-grow: 1;
+}
+.app-main {
+  height: calc(100vh - 66px);
+  overflow: auto;
+  background-color: #f6f8f9;
+  width: 100%;
 }
 </style>
